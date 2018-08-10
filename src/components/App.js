97a694a067df9ce48ba0169da22ef35fbba19839
Israel Vinicius <israelvinicius14@hotmail.com>
2018-08-10 13:08:03 -0300

    nao aparece nome do estadio

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import PlacarContainer from './PlacarContainer';
+
+const dados = {
+    partida: {
+        estadio: "Maracana/RJ",
+        data: "01/06/2016",
+        horario: "19h",
+    },
+    casa: {
+        nome: "vasco",
+    },
+    visitante: {
+        nome: "Flamengo",
+    },
+};
+
+export default class App extends React.Component{
+    render(){
+        return <PlacarContainer partida={dados.partida}
+                                casa={dados.casa}
+                                visitante={dados.visitante}/>;
+    }
+}
